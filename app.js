7e3bba52755632f59e6a1c447a860d7e4e00692d
SujayM <SujayMane@Hotmail.com>
2020-12-22 11:52:23 +0530

    Formatting with Shift + Alt + F

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const http = require('http');
-const path = require('path');
+const http = require("http");
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const adminRouter = require("./routes/admin");
 const shopRouter = require("./routes/shop");
@@ -12,34 +12,33 @@ const defaultRoute = require("./routes/default");
 const app = express();
 
 // For ejs
-app.set('view engine','ejs');
-app.set('views',path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname,'public')));
-
-app.use('/',(req, res, next) => {
-    console.log("This always Executes !!!");
-    console.log(req.body);
-    next();
+app.use("/", (req, res, next) => {
+  console.log("This always Executes !!!");
+  console.log(req.body);
+  next();
 });
 
 app.use(adminRouter.routes);
 app.use(shopRouter);
-app.use('/user/',userRouter);
+app.use("/user/", userRouter);
 app.use(defaultRoute);
 
-app.use((req,res,next)=>{
-    //res.status(404).send("<h1>Page Not Found!!!!</h1>")
-    //res.status(404).sendFile(path.join(__dirname,'views','pageNotFound.html')); // This is HTML page 
-    res.status(404).render("pageNotFound",{pageName:'404',pageTitle:'Page Not Found'});
+app.use((req, res, next) => {
+  //res.status(404).send("<h1>Page Not Found!!!!</h1>")
+  //res.status(404).sendFile(path.join(__dirname,'views','pageNotFound.html')); // This is HTML page
+  res
+    .status(404)
+    .render("pageNotFound", { pageName: "404", pageTitle: "Page Not Found" });
 });
-
-
 
 const server = http.createServer(app);
 
-server.listen(3000, function(){
-    console.log("Server started !!!");
+server.listen(3000, function () {
+  console.log("Server started !!!");
 });
--- routes/shop.js
+++ routes/shop.js
@@ -1,19 +1,26 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const rootPath = require('../util/path');
+const rootPath = require("../util/path");
 
 const router = express.Router();
 
-const adminRoute = require('./admin');
+const adminRoute = require("./admin");
 
-router.use('/catelog',(req, res, next) =>{
-    console.log("This is catelog !!!");
-    //res.sendFile(path.join(rootPath,'views','catelog.html'));
-    const products = adminRoute.products;
-    console.log("Product list" + products);
-    res.render('catelog',{pageTitle:'Catelog', prods:products, doctTitle:"Shopping Catalog", pageName:'catelog', hasProduct: products.length > 0, catelog:true});
+router.use("/catelog", (req, res, next) => {
+  console.log("This is catelog !!!");
+  //res.sendFile(path.join(rootPath,'views','catelog.html'));
+  const products = adminRoute.products;
+  console.log("Product list" + products);
+  res.render("catelog", {
+    pageTitle: "Catelog",
+    prods: products,
+    doctTitle: "Shopping Catalog",
+    pageName: "catelog",
+    hasProduct: products.length > 0,
+    catelog: true,
+  });
 });
 
 module.exports = router;
