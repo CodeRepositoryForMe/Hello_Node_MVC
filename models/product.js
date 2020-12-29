97de1d1252bb2455e72f974f2c4e275bb2e5cb4e
SujayM <SujayMane@Hotmail.com>
2020-12-30 00:57:49 +0530

    get / post data in json file

--- models/product.js
+++ models/product.js
@@ -1,8 +1,27 @@
 const fs = require("fs");
 const path = require("path");
 
-//const products = [];
+const filePath = path.join(
+  path.dirname(require.main.filename),
+  "data",
+  "products.json"
+);
 
+//const products = [];
+const getProductFromFile = (callback) => {
+  fs.readFile(filePath, (err, fileContent) => {
+    console.log("Reading file ***");
+    if (err) {
+      callback([]);
+    }
+    //console.log(JSON.parse(fileContent));
+    let products = [];
+    if (fileContent) {
+      products = JSON.parse(fileContent);
+    }
+    callback(products);
+  });
+};
 module.exports = class product {
   // constructor(productTitle){
   //     this.title = productTitle;
@@ -16,46 +35,15 @@ module.exports = class product {
   }
 
   save() {
-    console.log("Path ---- ");
-    console.log(require.main.filename);
-    const filePath = path.join(
-      path.dirname(require.main.filename),
-      "data",
-      "products.json"
-    );
-    fs.readFile(filePath, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        if (fileContent) {
-          products = JSON.parse(fileContent);
-        }
-        products.push(this);
-        fs.writeFile(filePath, JSON.stringify(products), (err) => {
-          console.log(err);
-        });
-      } else {
+    getProductFromFile((products) => {
+      products.push(this);
+      fs.writeFile(filePath, JSON.stringify(products), (err) => {
         console.log(err);
-      }
+      });
     });
   }
 
   static getAllProducts(callback) {
-    const filePath = path.join(
-      path.dirname(require.main.filename),
-      "data",
-      "products.json"
-    );
-    fs.readFile(filePath, (err, fileContent) => {
-      console.log("Reading file ***");
-      if (err) {
-        callback([]);
-      }
-      //console.log(JSON.parse(fileContent));
-      let products = [];
-      if (fileContent) {
-        products = JSON.parse(fileContent);
-      }
-      callback(products);
-    });
+    getProductFromFile(callback);
   }
 };
